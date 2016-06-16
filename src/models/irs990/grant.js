cb3b7412a4c90a4fe3083dfff76f592a788d99c2
Chris Zubak-Skees <chriszs@gmail.com>
2016-06-16 03:31:41 -0400

    No limits, just right

--- src/models/irs990/grant.js
+++ src/models/irs990/grant.js
@@ -1,23 +1,23 @@
 module.exports = function(sequelize, DataTypes) {
     var Grant = sequelize.define('irs990_grant', {
-        tax_year: DataTypes.STRING,
-        filer_ein: DataTypes.STRING,
-        recipient_name_1: DataTypes.STRING,
-        recipient_name_2:  DataTypes.STRING,
-        recipient_name2:  DataTypes.STRING,
-        recipient_street_1: DataTypes.STRING,
-        recipient_street_2: DataTypes.STRING,
-        recipient_city: DataTypes.STRING,
-        recipient_state: DataTypes.STRING,
-        recipient_zip: DataTypes.STRING,
-        recipient_country: DataTypes.STRING,
-        recipient_ein: DataTypes.STRING,
-        recipient_tax_section: DataTypes.STRING,
+        tax_year: DataTypes.TEXT,
+        filer_ein: DataTypes.TEXT,
+        recipient_name_1: DataTypes.TEXT,
+        recipient_name_2:  DataTypes.TEXT,
+        recipient_name2:  DataTypes.TEXT,
+        recipient_street_1: DataTypes.TEXT,
+        recipient_street_2: DataTypes.TEXT,
+        recipient_city: DataTypes.TEXT,
+        recipient_state: DataTypes.TEXT,
+        recipient_zip: DataTypes.TEXT,
+        recipient_country: DataTypes.TEXT,
+        recipient_ein: DataTypes.TEXT,
+        recipient_tax_section: DataTypes.TEXT,
         cash_amt: DataTypes.DECIMAL(12,2),
         non_cash_amt: DataTypes.DECIMAL(12,2),
-        non_cash_desc: DataTypes.STRING,
-        valuation_method: DataTypes.STRING,
-        purpose: DataTypes.STRING
+        non_cash_desc: DataTypes.TEXT,
+        valuation_method: DataTypes.TEXT,
+        purpose: DataTypes.TEXT
     });
 
     return Grant;
