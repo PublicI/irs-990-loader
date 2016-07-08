2805977c4350cbfc4e0323d83b3105fa9a955819
Chris Zubak-Skees <chriszs@gmail.com>
2016-07-08 19:46:54 -0400

    Add EO load SQL

---
+++ src/loadeo.sql
@@ -0,0 +1,4 @@
+\copy irs990_orgs FROM '/Users/cskees/Desktop/irs-990-loader/src/data/eo/eo1.csv' CSV HEADER;
+\copy irs990_orgs FROM '/Users/cskees/Desktop/irs-990-loader/src/data/eo/eo2.csv' CSV HEADER;
+\copy irs990_orgs FROM '/Users/cskees/Desktop/irs-990-loader/src/data/eo/eo3.csv' CSV HEADER;
+\copy irs990_orgs FROM '/Users/cskees/Desktop/irs-990-loader/src/data/eo/eo4.csv' CSV HEADER;
