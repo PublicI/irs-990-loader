4e828967a82f40219060f5c8543e1769742e3387
Chris Zubak-Skees <chriszs@gmail.com>
2019-01-13 23:02:12 -0500

    Add another decimal to org_compensation for people, should close #2

--- src/models/irs990/person.js
+++ src/models/irs990/person.js
@@ -12,7 +12,7 @@ module.exports = function(sequelize, DataTypes) {
         person_zip: DataTypes.TEXT,
         hours: DataTypes.DECIMAL(12,2),
         related_org_hours: DataTypes.DECIMAL(12,2),
-        org_compensation: DataTypes.DECIMAL(12,2),
+        org_compensation: DataTypes.DECIMAL(13,2),
         related_org_compensation: DataTypes.DECIMAL(12,2),
         other_compensation: DataTypes.DECIMAL(12,2),
         benefit_contribution: DataTypes.DECIMAL(12,2),
