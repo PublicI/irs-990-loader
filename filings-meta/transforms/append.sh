c7536706d76837174af902071ff0b599b067e84a
Chris Zubak-Skees <chriszs@gmail.com>
2019-01-17 12:39:31 -0500

    Fix single field with unescaped comma

--- filings-meta/transforms/append.sh
+++ filings-meta/transforms/append.sh
@@ -3,5 +3,5 @@
 for f in $(find $1*.csv); do
     base=$(basename $f .csv)
     year=${base#index_};
-    tr -d '\r' < $f | sed -e 's/$/,'$year'/' > $2/$base.csv
+    tr -d '\r' < $f | sed -e 's/MOSTYN FOUNDATION INC CO SILVERCREST ASSET ,AMAGEMENT/"MOSTYN FOUNDATION INC CO SILVERCREST ASSET ,AMAGEMENT"/' | sed -e 's/$/,'$year'/' > $2/$base.csv
 done
